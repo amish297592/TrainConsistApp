c1885502d4d91146741b4e30f71c715007e49932
Amish <your-email@example.com>
2026-04-01 09:22:42 +0530

    uc3  commited

--- Train/src/TrainUC3.java
+++ Train/src/TrainUC3.java
@@ -5,7 +5,7 @@ public class TrainUC3 {
 
     public static void main(String[] args) {
 
-        System.out.println("=== Train Consist Management App ===");
+        System.out.println("=== Train  Consist Management App ===");
 
         // Create HashSet for Bogie IDs
         Set<String> bogieIDs = new HashSet<>();
